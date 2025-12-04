cf9b441a8697550770ac9cf9b15ccf808fc68473
leoESILV <leonard92.j@gmail.com>
2025-12-04 12:23:49 +0100

    nouvel essaie de page web

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,18 @@
-import { defineConfig } from "vite";
+/*
+ * vite.config.js
+ * Copyright (C) 2025 mailitg <mailitg@maili-mba.local>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: "/my-3d-project/",
-});
+  base: '/my-3d-project/',
+  appType: 'spa',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    chunkSizeWarningLimit: 1500
+  },
+})
